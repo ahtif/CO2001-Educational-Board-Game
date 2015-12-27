a9aab6c74602404844b993f8ccea6981e1680867
ahtif <ahtif.a@hotmail.co.uk>
2015-12-27 19:09:05 +0000

    Added Oval class

---
+++ Oval.java
@@ -0,0 +1,26 @@
+public class Oval {
+	int x,y,width,height;
+
+	public Oval(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+}
